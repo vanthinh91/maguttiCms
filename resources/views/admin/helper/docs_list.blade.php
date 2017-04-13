cd55037d792fa137297f391eb2758707765ead61
Marco Asperti <marco.a@gfstudio.com>
2017-04-13 11:50:13 +0200

    add order by sor in  admin doc Gallery media list

--- resources/views/admin/helper/docs_list.blade.php
+++ resources/views/admin/helper/docs_list.blade.php
@@ -1,5 +1,5 @@
 <ul class="thumbnails ui-sortable mf0" id="simpleDocGallery">
-    @forelse($article->media->where('collection_name','docs')->all() as $media)
+    @forelse($article->media()->where('collection_name','docs')->orderBy('sort')->get() as $media)
         <li id="box_media_{!! $media->id!!}" class="thumbnail pf10 mb14">111
 
             <div class="pull-right">
