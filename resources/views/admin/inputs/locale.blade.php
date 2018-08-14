98923426c007f7ded47d7e146e0f3d662f2bcf97
marco asperti <marcoasperti@gmail.com>
2018-08-14 14:25:23 +0200

    upgrade to 5.6.33

--- resources/views/admin/inputs/locale.blade.php
+++ resources/views/admin/inputs/locale.blade.php
@@ -3,7 +3,7 @@
 		<option value=""></option>
 	@endif
 	@foreach (config('app.locales') as $_code => $_name)
-		<option value="{{$_code}}" {{($value = $_code)? 'selected': ''}}>
+		<option value="{{$_code}}" {{($value == $_code)? 'selected': ''}}>
 			{{$_name}}
 		</option>
 	@endforeach
