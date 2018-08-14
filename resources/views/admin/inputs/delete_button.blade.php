ad3edb17afec9c7bb12c557a1ffa3004808c2031
marco asperti <marcoasperti@gmail.com>
2018-08-14 14:02:34 +0200

    fix delete image button

---
+++ resources/views/admin/inputs/delete_button.blade.php
@@ -0,0 +1,5 @@
+<a href="#" rel="tooltip" class="btn btn-danger ml10"
+            data-original-title="{{trans('admin.message.delete_item')}}"
+            onclick="window.Cms.deleteImages(this)" id="{{"delete-".$key}}-{{$id}}">
+            <i class="fa fa-trash big"></i>
+</a>
