456cce3210dcb8f34523615b06d229b9abbcd10e
Marco Asperti <marco.a@gfstudio.com>
2018-08-27 16:48:10 +0200

    typo fix

--- resources/views/admin/inputs/delete_button.blade.php
+++ resources/views/admin/inputs/delete_button.blade.php
@@ -3,3 +3,13 @@
             onclick="window.Cms.deleteImages(this)" id="{{"delete-".$key}}-{{$id}}">
             <i class="fa fa-trash big"></i>
 </a>
+
+
+<a id="delete-{{$key}}-{{$id}}"
+   class="btn btn-danger media-delete"
+   href="#"
+   rel="tooltip"
+   data-original-title="{{trans('admin.message.delete_item')}}"
+   onclick="window.Cms.deleteImages(this)">
+   {{icon('trash')}}
+</a>
