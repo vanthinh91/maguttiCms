13c5977907566d23b8f9d985d2a2c65b537b6287
marco <marcoasperti@gmail.com>
2019-05-04 15:02:57 +0200

    add  public path

--- config/maguttiCms/admin/path.php
+++ config/maguttiCms/admin/path.php
@@ -5,6 +5,7 @@ return [
 	'img_repository' => 'media/images/',
 	'img_save'       => 'media/images/cache/',
 	'doc_repository' => 'media/docs/',
+    'public'         => '',
 
 	'media_img_repository' => 'media/images/',
 	'media_doc_repository' => 'media/docs/',
