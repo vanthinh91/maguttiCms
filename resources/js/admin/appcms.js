ed64d13009940b2ea229d0a0ea26f8a8800eba18
marco <marcoasperti@gmail.com>
2019-05-26 19:52:57 +0200

    update to 5.8.18

--- resources/js/admin/appcms.js
+++ resources/js/admin/appcms.js
@@ -15,6 +15,7 @@ window.Vue = require('vue');
  *
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
+
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
