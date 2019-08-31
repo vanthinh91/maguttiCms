d5a51cb1b1a263c1656887a0e7f9817683b9f33d
marco <marcoasperti@gmail.com>
2019-08-31 12:21:24 +0200

    Add models tab to debug bar

--- config/debugbar.php
+++ config/debugbar.php
@@ -10,9 +10,14 @@ return [
      | Debugbar is enabled by default, when debug is set to true in app.php.
      | You can override the value by setting enable to true or false instead of null.
      |
+     | You can provide an array of URI's that must be ignored (eg. 'api/*')
+     |
      */
 
     'enabled' => env('DEBUGBAR_ENABLED', null),
+    'except' => [
+        'telescope*'
+    ],
 
     /*
      |--------------------------------------------------------------------------
@@ -74,7 +79,7 @@ return [
      |
      */
     'error_handler' => false,
-    
+
     /*
      |--------------------------------------------------------------------------
      | Clockwork integration
@@ -84,7 +89,7 @@ return [
      | Extension, without the server-side code. It uses Debugbar collectors instead.
      |
      */
-    'clockwork' => true,
+    'clockwork' => false,
 
     /*
      |--------------------------------------------------------------------------
@@ -105,7 +110,7 @@ return [
         'db'              => true,  // Show database (PDO) queries and bindings
         'views'           => true,  // Views with their data
         'route'           => true,  // Current route information
-        'auth'            => true, // Display Laravel authentication status
+        'auth'            => false, // Display Laravel authentication status
         'gate'            => true, // Display Laravel Gate checks
         'session'         => true,  // Display session data
         'symfony_request' => true,  // Only one can be enabled..
@@ -116,6 +121,8 @@ return [
         'logs'            => false, // Add the latest log messages
         'files'           => false, // Show the included files
         'config'          => false, // Display config settings
+        'cache'           => false, // Display cache events
+        'models'          => true, // Display models
     ],
 
     /*
@@ -137,7 +144,7 @@ return [
             'timeline'          => false,  // Add the queries to the timeline
             'explain' => [                 // Show EXPLAIN output on queries
                 'enabled' => false,
-                'types' => ['SELECT'],     // ['SELECT', 'INSERT', 'UPDATE', 'DELETE']; for MySQL 5.6.3+
+                'types' => ['SELECT'],     // // workaround ['SELECT'] only. https://github.com/barryvdh/laravel-debugbar/issues/888 ['SELECT', 'INSERT', 'UPDATE', 'DELETE']; for MySQL 5.6.3+
             ],
             'hints'             => true,    // Show hints for common mistakes
         ],
@@ -152,6 +159,9 @@ return [
         ],
         'logs' => [
             'file' => null
+        ],
+        'cache' => [
+            'values' => true // collect cache values
         ],
     ],
 
