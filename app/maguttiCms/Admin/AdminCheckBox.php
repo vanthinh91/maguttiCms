c1938b9cb0d04c04cb0c6f9220146fccd589469c
Marco Asperti <marco.a@gfstudio.com>
2020-02-03 09:09:29 +0100

    Fix  status on  button toogle

--- app/maguttiCms/Admin/AdminCheckBox.php
+++ app/maguttiCms/Admin/AdminCheckBox.php
@@ -15,14 +15,16 @@ class AdminCheckBox extends AdminFormComponent {
         $this->modelName = strtolower(class_basename($this->formObject->model));
         $booleanInputId = $key .'_'. $this->modelName .'_'. $this->formObject->model->id;
         //$formElement = Form::checkbox($key, 1 , $this->model->$key);
-        $activeNo = ($value != '1')? ' '.$this->active: '';
-        $activeYes = ($value == '1')? $this->active: '';
+        $activeNo   = ($value != '1')? ' '.$this->active: '';
+        $activeYes  = ($value == '1')? $this->active: '';
+        $checkedYes = ($value == '1')? 'checked': '';
+        $checkedNo  = ($value != '1')? 'checked': '';
         $this->html.="<div class=\"btn-group\" data-toggle=\"buttons\">\n";
         $this->html.=' <button type="button" class="btn btn-default '.$activeYes.'" onclick="$(\'#'.$booleanInputId.'\').val(1)">
-					<input type="radio" name="options" autocomplete="off" '.$activeYes.'>'.trans('admin.label.btn_yes').'
+					<input type="radio" name="options" autocomplete="off" '.$checkedYes.'>'.trans('admin.label.btn_yes').'
 				</button>';
         $this->html.=' <button type="button" class="btn btn-default '.$activeNo.'" onclick="$(\'#'.$booleanInputId.'\').val(0)">
-					<input type="radio" name="options" autocomplete="off" '.$activeNo.'> '.trans('admin.label.btn_no').'
+					<input type="radio" name="options" autocomplete="off" '.$checkedNo.'> '.trans('admin.label.btn_no').'
 				</button>';
         $this->html.="</div>\n";
         /* GF_ma aggiornato dopo Aggiunta Middleware ConvertEmptyStringsToNull metto di default 0 anzichè null */
