205c391431a6326f65f670bdc57571d004b2b5f0
marco <marcoasperti@gmail.com>
2020-02-17 21:14:57 +0100

    Create path_shared.php

---
+++ config/maguttiCms/admin/path_shared.php
@@ -0,0 +1,29 @@
+<?php
+return [
+    'repository' => 'public/media/',
+    'img_repository' => 'public/media/images/',
+    'img_save' => 'public/media/images/cache/',
+    'doc_repository' => 'media/docs/',
+
+    'media_img_repository' => 'public/media/images/',
+    'media_doc_repository' => 'public/media/docs/',
+
+    'cms' => 'public/cms/',
+    'cms_assets' => 'public/cms/',
+    'cms_js' => 'public/cms/js/',
+    'cms_css' => 'public/cms/css/',
+
+    'assets' => '',
+    'common_js' => 'js/',
+    'js_vendor' => 'js/vendor/',
+    'plugins' => 'plugins/',
+    'common_css' => 'css/',
+    'css_vendor' => 'css/vendor/',
+
+    'user_upload' => 'upload/',
+
+    'expor_data' => 'export/',
+    'shared' => 'shared_data/',
+
+
+];
