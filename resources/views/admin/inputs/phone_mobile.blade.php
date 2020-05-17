3bbfd816f8581e27cc240044622752cb6962ff31
marco <marcoasperti@gmail.com>
2020-05-17 15:24:21 +0200

    add phone_mobile component

---
+++ resources/views/admin/inputs/phone_mobile.blade.php
@@ -0,0 +1,14 @@
+<div class="row form-group ">
+    <div class="col-12  col-md-5 col-lg-5">
+        <input name="street" type="text" value="{{$model->phone}}" class=" form-control">
+    </div>
+    <div class="col-12  col-md-2 col-lg-2">
+        <label>
+            {{trans('admin.label.mobile')}} <mark>*</mark>
+        </label>
+    </div>
+    <div class="col-12  col-md-5 col-lg-5">
+        <input name="street" type="text" value="{{$model->mobile}}" class=" form-control">
+    </div>
+
+</div>
