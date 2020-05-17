2c78a8d339808a68f81314d8d6906495ed2949af
marco <marcoasperti@gmail.com>
2020-05-17 12:47:13 +0200

    Refactor AdminFormRequest in order to support multi fields validation rules

--- app/maguttiCms/Admin/Requests/AdminFormRequest.php
+++ app/maguttiCms/Admin/Requests/AdminFormRequest.php
@@ -3,42 +3,68 @@
 use Illuminate\Foundation\Http\FormRequest;
 use Input;
 
+/**
+ * Class AdminFormRequest
+ * @package App\maguttiCms\Admin\Requests
+ */
 class AdminFormRequest extends FormRequest
 {
-	/**
-	* Determine if the user is authorized
-	* to make this request.
-	*
-	* @return bool
-	*/
-	public function authorize()
-	{
-		return true;
-	}
+    /**
+     * Determine if the user is authorized
+     * to make this request.
+     *
+     * @return bool
+     */
+    protected $rules = [];
 
-	/**
-	* Get the validation rules that
-	* apply to the request.
-	*
-	* @return array
-	*/
-	public function rules()
-	{
-		$model = ($this::segment(2)=='create')? $this::segment(count($this::segments())): $this::segment(count($this::segments())-1);
+    public function authorize()
+    {
+        return true;
+    }
 
-		//Metodo Standard sarà rimosso
-		$rules = config('maguttiCms.admin.form_validation.'.$model);
+    /**
+     * Get the validation rules that
+     * apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
 
-		$curModel = getModelFromString(config('maguttiCms.admin.list.section.'.$model)['model']);
-		$obj = new $curModel;
+        $obj = $this->getModelObject();
+        foreach ($obj->getFieldspec() as $key => $field) {
+            if (data_get($field, 'validation') != '') {
+                $this->handleValidationRules($field['validation'], $key);
+            } else if (data_get($field, 'required') && in_array($key, $obj->getFillable())) {
+                $this->addRule($key);
+            }
+        }
+        return $this->rules;
+    }
 
-		foreach($obj->getFieldspec() as $key => $field) {
-			if (data_get($field,'validation') != '') {
-				$rules[$key] = $field['validation'];
-			} else if (data_get($field, 'required') && in_array($key, $obj->getFillable())) {
-				$rules[$key] = "required";
-			}
-		};
-		return $rules;
-	}
+    function handleValidationRules($validation_rules, $key)
+    {
+        if (is_array($validation_rules)) {
+            foreach ($validation_rules as $item => $value) {
+                $this->addRule($item, $value);
+            }
+        } else $this->addRule($key, $validation_rules);
+    }
+
+    function addRule($key, $validation_rules = 'required')
+    {
+        $this->rules[$key] = $validation_rules;
+    }
+
+    /**
+     * Resolve the model to validate
+     * from path
+     * @return mixed
+     */
+    function getModelObject()
+    {
+        $model = ($this::segment(2) == 'create') ? $this::segment(count($this::segments())) : $this::segment(count($this::segments()) - 1);
+        $curModel = getModelFromString(config('maguttiCms.admin.list.section.' . $model)['model']);
+        return new $curModel;
+    }
 }
