07a82435aafac1b230cdda1bf9ed20a09ea4c05f
marco <marcoasperti@gmail.com>
2020-08-04 21:10:02 +0200

    Fix . depracated array_key_exists

--- resources/views/admin/inputs/file.blade.php
+++ resources/views/admin/inputs/file.blade.php
@@ -1,4 +1,5 @@
 <div class="form-file">
-	<input id="file_{{$key}}" type="file" name="{{$key}}" data-selected-caption="{{trans('admin.label.file_count')}}" {!!(array_key_exists('accept', $properties))? 'accept="'.$properties['accept'].'"': ''!!}>
+	<input id="file_{{$key}}" type="file" name="{{$key}}" data-selected-caption="{{trans('admin.label.file_count')}}"
+			{!!  (data_get($properties,'accept')) ? 'accept="'.$properties['accept'].'"' : ''!!}>
 	<label for="file_{{$key}}">{{trans('admin.label.upload_file')}}</label>
 </div>
