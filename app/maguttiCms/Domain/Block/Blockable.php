9b0dc33e3bfa12743a3320a20c369a82314032f6
marco <marcoasperti@gmail.com>
2020-08-11 14:11:54 +0200

    Added Block and BlockById method

--- app/maguttiCms/Domain/Block/Blockable.php
+++ app/maguttiCms/Domain/Block/Blockable.php
@@ -4,8 +4,28 @@ namespace App\maguttiCms\Domain\Block;
 
 trait Blockable
 {
+    /**
+     * GET ALL MODEL BLOCKS
+     * @return mixed
+     */
     public function blocks()
     {
         return $this->morphMany('App\Block', 'model');
+    }
+
+    /**
+     * GET MODEL BLOCK BY ID
+     * @param $id
+     * @return mixed
+     */
+    public function blockById($id)
+    {
+
+        return $this->block($id);
+    }
+
+    public function block($id,$field='id')
+    {
+        return $this->blocks()->where($field,$id)->first();
     }
 }
