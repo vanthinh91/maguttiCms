07d07bc8dcf1461a837aab136a0bff91cf531339
marco <marcoasperti@gmail.com>
2020-08-21 16:21:29 +0200

    Categories and Products as blade components

--- resources/views/components/website/products/item.blade.php
+++ resources/views/components/website/products/item.blade.php
@@ -1,8 +1,8 @@
 <article class="card box-shadow products__card">
     <img class="card-img-top" src="{{ ImgHelper::get($product->image, config('maguttiCms.image.defaults')) }}" alt="{{ $product->title }}">
     <div class="card-body bg-color-3">
+        <h4 class="card-title text-primary"><span class="text-uppercase">{{ $product->title }}</span> <span class="card-code text-muted"> - {{ trans('store.product.code') }}: {{ $product->code }}</span> </h4>
         <div class="card-price">{{ StoreHelper::formatProductPrice($product) }}</div>
-        <h4 class="card-title text-primary">{{ $product->title }} <span class="card-code text-muted"> - {{ trans('store.product.code') }}: {{ $product->code }}</span> </h4>
         <a href="{{ $product->getPermalink() }}" class="btn btn-sm btn-outline-accent mt-2">
             {{ trans('website.see') }} {{ icon('fas fa-caret-right') }}
         </a>
