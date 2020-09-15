1182a9adc7301482e2e9ce465fb68f90cb9a39f3
Marco Asperti <marco.a@gfstudio.com>
2020-09-15 08:59:11 +0200

    Category Page Layout

--- resources/views/website/category.blade.php
+++ resources/views/website/category.blade.php
@@ -4,15 +4,6 @@
 			<h1 class="page-breadcrumb__item">{{$category->title}}</h1>
 		</div>
 	</x-website.ui.breadcrumbs>
-	<x-website.partials.section :class="'bg-accent py-5'" classCaption="text-primary">
-		asaSAsa
-		@foreach ($products as $product)
-			<div class="col-12 col-sm-6 col-lg-3">
-				<x-website.products.item :product="$product"></x-website.products.item>
-			</div>
-		@endforeach
-	</x-website.partials.section>
-
 	<section class="py-2 py-md-4">
 		<div class="container">
 			<div class="row">
