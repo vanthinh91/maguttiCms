dd1e6e2b3f97a12f023dbeff4d6e31a1aaff6965
marco <marcoasperti@gmail.com>
2020-10-18 12:52:52 +0200

    Update To Laravel 8

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-/*Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});*/
-
 Route::group(['prefix' => 'v1'], function () {
     Route::get('article', '\App\maguttiCms\Api\V1\Controllers\ArticlesController@index');
-    //Route::post('user', '\App\maguttiCms\Api\V1\Controllers\UserController@create');
 });
