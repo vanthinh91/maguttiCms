1b201ac8630410ed0e418264123666f918dc102c
marco <marcoasperti@gmail.com>
2020-10-21 22:45:30 +0200

    Added Page title template

---
+++ resources/views/components/website/partials/page-title.blade.php
@@ -0,0 +1,6 @@
+@if(isset($subtitle))
+    <h2 class="text-accent">{{ $subtitle }}</h2>
+@endif
+<h1 class="text-primary">{{ $slot }}</h1>
+
+
--- resources/views/components/website/ui/breadcrumbs.blade.php
+++ resources/views/components/website/ui/breadcrumbs.blade.php
@@ -1,7 +1,9 @@
 <section {{ $attributes->merge(['class' => 'p-0']) }}>
     <div class="container">
         <div class="row">
-            <div class="col-12">{{$slot}}</div>
+            <div class="col-12">
+                {{$slot}}
+            </div>
         </div>
     </div>
 </section>
