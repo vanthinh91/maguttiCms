2453bc6e2d7dad9f8ec0981c87e863be342bb338
marco <marcoasperti@gmail.com>
2020-11-07 11:12:57 +0100

    Added ForceSSLMiddleware to force laravel to serve  resouces with HTTPS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,12 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        /*
+        |--------------------------------------------------------------------------
+        | LaraCms Middleware
+        |--------------------------------------------------------------------------
+        */
+        \App\maguttiCms\Middleware\ForceSSLMiddleware::class
     ];
 
     /**
