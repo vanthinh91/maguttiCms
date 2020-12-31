aab6094cc1b75480e5bed94b336bdce648856a70
marco <marcoasperti@gmail.com>
2020-12-31 14:47:38 +0100

    Refactor email notification template

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,11 @@
 <tr>
-    <td class="header">
-        <a href="{{ $url }}">
-            {{ $slot }}
-        </a>
-    </td>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+@else
+ <img src="{!! asset('/website/images/email/email_header.png')!!}"  class="logo"  alt="{{ $slot }}"/>
+@endif
+</a>
+</td>
 </tr>
