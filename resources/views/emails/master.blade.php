d255ee62887dc6d7c55b865f1af5b6f21693599a
marco <marcoasperti@gmail.com>
2020-12-31 15:02:17 +0100

    Refactor. email template layout

--- resources/views/emails/master.blade.php
+++ resources/views/emails/master.blade.php
@@ -10,13 +10,13 @@
         }
     </style>
 </head>
-<body>
-<table class="table table-mail" style="width:650px;margin-top:0px;" align="center">
+<body style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; -webkit-text-size-adjust: none; background-color: #edf2f7; color: #718096; height: 100%; line-height: 1.4; margin: 0; padding: 0; width: 100% !important;">
+<table class="table table-mail" style="width:575px; margin-top:25px" align="center">
     <tr>
         <td  style="padding:0px;width:650px" bgcolor="#ffffff" align="left">
-            <table class="table_inner" align="left">
+            <table class="table_inner" align="left"  style="padding:15px">
                 <tr>
-                    <td align="left" class="logo" style="padding:0px;border-bottom:1px solid #6F6F6F;">
+                    <td align="left" class="logo" style="padding:0px;border-bottom:1px solid #edf2f7;">
                         <img src="{!! asset('/website/images/email/email_header.png')!!}"   border="0"/>
                     </td>
                 </tr>
@@ -26,7 +26,7 @@
                     </td>
                 </tr>
                 <tr bgcolor="#ffffff" >
-                    <td class="footer" style="border-top:1px solid #6F6F6F;padding:7px 0px 0px 0px;font-size:14px;" bgcolor="#ffffff" bgcolor="#ffffff">
+                    <td class="footer" style="border-top:1px solid #edf2f7;padding:7px 0px 0px 0px;font-size:14px;" bgcolor="#ffffff" bgcolor="#ffffff">
                         {{ config('maguttiCms.website.option.email.footer') }}
                     </td>
                 </tr>
