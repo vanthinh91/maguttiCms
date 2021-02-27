0112450ab0802e8801da357ae631fcef1d5c0efa
Marco Asperti <marcoasperti@gmail.com>
2021-02-27 19:10:00 +0100

    fix.billing_address not setted

--- app/maguttiCms/Domain/Store/Action/UpdateCartAddressAction.php
+++ app/maguttiCms/Domain/Store/Action/UpdateCartAddressAction.php
@@ -21,11 +21,10 @@ class UpdateCartAddressAction
     {
         $shipping_address = (new AddAddressToCartAction($this->cart))->handle($request);
         $this->cart->update_cart_address('shipping_address_id', $shipping_address->id);
-
         if($request->has('shippingAsBilling')){
             $billing_address = (new AddAddressToCartAction($this->cart))->handle($request,'billing');
             $this->cart->update_cart_address('billing_address_id', $billing_address->id);
         }
+        else $this->cart->update_cart_address('billing_address_id', null);
     }
-
 }
