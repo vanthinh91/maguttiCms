9e168c42ee4f9eeb1a40fbaffddb8e1d3b70018d
Marco Asperti <marcoasperti@gmail.com>
2021-04-10 19:23:01 +0200

    Update side_bar_action.blade.php

--- resources/views/admin/common/side_bar_action.blade.php
+++ resources/views/admin/common/side_bar_action.blade.php
@@ -1,12 +1,12 @@
 <x-admin.card.side-bar id="edit-sidebar">
     <x-slot name="title">{!! trans('admin.label.actions')!!}</x-slot>
     <ul>
-        @if ($pageConfig['actions']['copy']==1 && $article->id!='')
+        @if (data_get($pageConfig,'actions.copy')==1 && $article->id!='')
             <li>
                 <a href="{{ma_get_admin_copy_url($article)}}">{{icon('copy')}}{!! trans('admin.label.copy')!!}</a>
             </li>
         @endif
-        @if ($pageConfig['actions']['create']==1)
+        @if (data_get($pageConfig,'actions.create')==1)
             <li>
                 <a href="{{ma_get_admin_create_url($article)}}">{{icon('plus')}}{!! trans('admin.message.add_new_item')!!}</a>
             </li>
@@ -14,7 +14,7 @@
         <li>
             <a href="{{ma_get_admin_list_url($article)}}">{{icon('reply')}}{!! trans('admin.message.back_to_list')!!}</a>
         </li>
-        @if ($pageConfig['actions']['preview']==1)
+        @if (data_get($pageConfig,'actions.preview')==1)
             <li>
                 <a href="{{ma_get_admin_preview_url($article)}}"
                    target="_new">{{icon('eye')}}{!! trans('admin.message.view_page')!!}</a>
