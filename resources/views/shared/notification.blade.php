0ad2d10e54d4373ac4c6f945e2daba03b59ebd7d
Marco Asperti <marcoasperti@gmail.com>
2021-05-27 01:32:05 +0200

    Update AlertBox to Bootstrap 5

--- resources/views/shared/notification.blade.php
+++ resources/views/shared/notification.blade.php
@@ -1,13 +1,13 @@
 @if (session('success'))
-	<div class="text-center alert alert-success">
-		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">{{icon('times')}}</button>
+	<div class="text-center alert alert-success alert-dismissible">
+		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 		{{icon('check-circle', 'fa-3x')}}
 		<p>{!! session('success') !!}</p>
 	</div>
 @endif
 @if ($errors->any())
-	<div class='text-center alert alert-danger'>
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close">{{icon('times')}}</button>
+	<div class='text-center alert alert-danger alert-dismissible'>
+		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 		{{icon('times-circle', 'fa-3x')}}
 		@foreach ( $errors->all() as $error)
 			<p>{{ $error }}</p>
@@ -15,22 +15,22 @@
 	</div>
 @endif
 @if (session('error'))
-	<div class="text-center alert alert-danger">
-		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">{{icon('times')}}</button>
+	<div class="text-center alert alert-danger alert-dismissible">
+		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 		{{icon('times-circle', 'fa-3x')}}
 		<p>{!! session('error') !!}</p>
 	</div>
 @endif
 @if (session('warning'))
-	<div class="text-center alert alert-warning">
-		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">{{icon('times')}}</button>
+	<div class="text-center alert alert-warning alert-dismissible">
+		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 		{{icon('exclamation-triangle', 'fa-3x')}}
 		<p>{!! session('warning') !!}</p>
 	</div>
 @endif
 @if (session('status'))
-	<div class="text-center alert alert-info">
-		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">{{icon('times')}}</button>
+	<div class="text-center alert alert-info alert-dismissible">
+		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 		{{icon('exclamation-circle', 'fa-3x')}}
 		<p>{!! session('status') !!}</p>
 	</div>
