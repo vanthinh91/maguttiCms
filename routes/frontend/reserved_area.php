2d1cf2ba1c95b6209ffd9f6d4d2586bf9493ca51
Marco Asperti <marcoasperti@gmail.com>
2021-08-15 01:13:52 +0200

    Update reserved_area.php

--- routes/frontend/reserved_area.php
+++ routes/frontend/reserved_area.php
@@ -6,13 +6,13 @@ use App\maguttiCms\Website\Controllers\UpdatePasswordController;
 use App\maguttiCms\Website\Controllers\UserAvatarApiController;
 
 Route::get('/dashboard', [ReservedAreaController::class, 'dashboard'])->name('user.dashboard');
-Route::get('/profile', [ReservedAreaController::class, 'profile'])->name('user.profile');
-Route::post('/profile', [ReservedAreaController::class, 'update_profile']);
+Route::get('/profile',   [ReservedAreaController::class, 'profile'])->name('user.profile');
+Route::post('/profile',  [ReservedAreaController::class, 'update_profile']);
 Route::post('/change-password', [UpdatePasswordController::class, 'update_password'])->name('user.change-password');
 Route::get('/order-detail/{order}', [OrderControllers::class, 'show'])->name('order.detail');
 
 Route::get('/address-new', [ReservedAreaController::class, 'addressNew']);
-Route::post('/address-new', [ReservedAreaController::class, 'addressCreate']);
+Route::post('/address-new',[ReservedAreaController::class, 'addressCreate']);
 
-Route::post('/avatar', [UserAvatarApiController::class,'create'])->name('user.avatar-upload');
+Route::post('/avatar',   [UserAvatarApiController::class,'create'])->name('user.avatar-upload');
 Route::delete('/avatar', [UserAvatarApiController::class,'delete'])->name('user.avatar-delete');
