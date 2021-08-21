332ebbab0394fa7c513b3ab93de38ed824842dba
Marco Asperti <marcoasperti@gmail.com>
2021-08-21 13:50:31 +0200

    Disabled preventLazyLoading check

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        Model::preventLazyLoading(! app()->isProduction());
+        //Model::preventLazyLoading(! app()->isProduction());
     }
 
     /**
