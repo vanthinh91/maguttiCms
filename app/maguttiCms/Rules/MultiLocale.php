dcd41cb684a9141b9bf25972051b825af7cc64a3
Marco Asperti <marcoasperti@gmail.com>
2021-09-04 00:05:27 +0200

    Moved under App\maguttiCms\Rules

---
+++ app/maguttiCms/Rules/MultiLocale.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\maguttiCms\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+/**
+ * @property string current_locale
+ */
+class MultiLocale implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+        $this->current_locale = app()->getLocale();
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        //
+        foreach (config('app.locales') as $locale => $code) {
+           // skip the current locale
+           if($locale!=$this->current_locale){
+                $field_locale =$attribute.'_'.$locale;
+                if(request()->get($field_locale)=='')return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+       return 'All the :attribute fields are required';
+    }
+}
